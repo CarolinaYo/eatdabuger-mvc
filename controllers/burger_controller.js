0eed68d799e667165c6d887d82f8a3d7d2876581
CarolY <mon_alisa75@yahoo.com>
2020-12-03 15:22:06 -0800

    Add delete route.

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -54,7 +54,26 @@ router.put("/api/burgers/:id", function(req, res) {
 });
 
 //deleting from devoured list
+router.delete("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
 
+  console.log("condition ", condition);
+
+  burger.deleteOne(
+    {
+      devoured: 1
+    },
+    condition,
+    function(result) {
+      if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
+      res.status(200).end();
+
+    }
+  );
+});
 
 
 // Export routes for server.js to use.
