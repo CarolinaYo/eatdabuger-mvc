fbf4007333b804a6a339fe4d945da80039c36610
CarolY <mon_alisa75@yahoo.com>
2020-12-03 15:37:03 -0800

    Delete error correction.

--- models/burger.js
+++ models/burger.js
@@ -1,6 +1,5 @@
 // Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
-const { delete } = require("../controllers/burger_controller.js");
 
 var burger = {
   selectAll: function(cb) {
@@ -19,11 +18,12 @@ var burger = {
       cb(res);
     });
   },
-  deleteOne: function(objColVals, condition, cb) {
-    orm.updateOne("burgers", objColVals, condition, function(res) {
+  deleteOne: function(condition, cb) {
+    orm.updateOne("burgers", condition, function(res) {
       cb(res);
     });
-};
+  }
+}
 
 // Export the database functions for the controller (burger_controller.js).
 module.exports = burger;
