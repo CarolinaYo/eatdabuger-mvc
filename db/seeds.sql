4d010da9c6e4e10b1d44484a7f1e5932dd5907d3
CarolY <mon_alisa75@yahoo.com>
2020-12-03 13:58:53 -0800

    Data correction.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 INSERT INTO burgers (burger_name) VALUES ('Veggie burger');
 INSERT INTO burgers (burger_name) VALUES ('Double-Cheese Burger');
 INSERT INTO burgers (burger_name) VALUES ('Quarter-pounder Burger');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Classic Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Classic Burger', true)
